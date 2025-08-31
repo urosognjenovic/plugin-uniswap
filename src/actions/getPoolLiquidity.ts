5e0700d3efd754987118ba341da71285eafcae75
Uros Ognjenovic <urosognjenovic17@gmail.com>
2025-08-31 09:52:08 +0200

    Remove comments with test poolIds

--- src/actions/getPoolLiquidity.ts
+++ src/actions/getPoolLiquidity.ts
@@ -40,12 +40,6 @@ export const getPoolLiquidityAction: Action = {
     callback: HandlerCallback
   ): Promise<ActionResult> => {
     try {
-      // Ethereum poolId for testing
-      // const poolId = "0x21C67E77068DE97969BA93D4AAB21826D33CA12BB9F565D8496E8FDA8A82CA27";
-      // Avalanche poolId for testing
-      // const poolId = "0x378035D348FB3EB5535DF6775AC96A7FC03142F663D7002F0B98555021753A04";
-      // Base poolId for testing
-      // const poolid = "0x74B1EB0EB9068ED54B6B9D55673F7DE8FAC3299CE7E3DF916E0172676D225A1A";
       const userMessage = message.content.text;
       let poolId: String | undefined = "";
 
